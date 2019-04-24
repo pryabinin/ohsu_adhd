c068ae084ac6da5cb8ea5a998f2723260e2dc6c2
pryabinin <pryabinin@users.noreply.github.com>
2019-04-24 15:14:17 -0700

    Add files via upload

--- EWAS/EWAS_results/main_model_analysis.R
+++ EWAS/EWAS_results/main_model_analysis.R
@@ -3,14 +3,14 @@ library(limma)
 load("adjusted_betas.Rdata")
 
 # Perform analysis of full model:
-# Methylation (beta values adjusted for cell type) ~ 1 + age + sex + status + 3 Genomic PC's
+# Methylation (beta values adjusted for cell type) ~ 1 + age + sex + status + 3 Genomic PC's + Smoking Score + Medication History
 sample.annot <- sample.annot[match(colnames(adj.betas),sample.annot$patientID),]
 
 temp.status <- factor(sample.annot$status, levels=c("control","case"))
 temp.sex <- factor(sample.annot$sex, levels=c("Female","Male"))
-temp.design <- model.matrix(~temp.status+temp.sex+sample.annot$Age+sample.annot$PC1+sample.annot$PC2+sample.annot$PC3)
+temp.design <- model.matrix(~temp.status+temp.sex+sample.annot$Age+sample.annot$PC1+sample.annot$PC2+sample.annot$PC3+sample.annot$smoking.score+sample.annot$Medication)
 results.fit <- lmFit(adj.betas, temp.design)
 results.fit <- eBayes(results.fit)
 
 # Save the results object
-save(results.fit,file="full_model_fit_object.Rdata")
+save(results.fit,file="main_model_fit_object.Rdata")
